8e766c6ccc641efb43b2cb7a0c2d06cc6dd9ce8f
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-12-10 18:57:50 +0800

    :sparkles: prisma skeleton

---
+++ lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+
+let prisma: PrismaClient | undefined
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!prisma) {
+    prisma = new PrismaClient()
+  }
+  prisma = prisma
+}
+
+export default prisma
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import prisma from 'lib/prisma'
+import { shares } from '@prisma/client'
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return <div>Hello</div>
+}
+
+export async function getStaticProps() {
+  let data = null
+
+  if (prisma) {
+    const response = (await prisma.shares.findFirst({
+      where: { id: 228 },
+    })) as shares
+
+    const { created_at, updated_at, ...rest } = response
+    data = rest
+  }
+
+  return {
+    props: {
+      some: 'stuff',
+      shares: data,
+    },
+  }
 }
 
 export default Home
