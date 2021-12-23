de7521415cc58e64cd1c4ffb6352576aa3eb9860
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-12-23 09:06:44 +0800

    :sparkles: add react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
