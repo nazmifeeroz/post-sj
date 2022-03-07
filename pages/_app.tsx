438c740c743774352070a2f3d9de357502c83803
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2022-03-08 07:17:53 +0800

    :passport_control: only authenticated users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import LoadingIndicator from '_components/LoadingIndicator'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Silicon Jungles Alumni" />
-      </Head>
-      <LoadingIndicator />
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Silicon Jungles Alumni" />
+        </Head>
+        <LoadingIndicator />
+        <Component {...pageProps} />
+      </SessionProvider>
     </ChakraProvider>
   )
 }
